5419d64236584e663ac26a83d6ded11a2e6f67ba
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-12-24 16:36:48 +0100

    Add @jnxplus/nx-gradle plugin

---
+++ nx-gradle/settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+  val springBootVersion: String by settings
+  val springDependencyManagementVersion: String by settings
+  val kotlinVersion: String by settings
+  val jnxplusGradlePluginVersion: String by settings
+  plugins {
+    id("org.springframework.boot") version springBootVersion
+    id("io.spring.dependency-management") version springDependencyManagementVersion
+    id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("io.github.khalilou88.jnxplus") version jnxplusGradlePluginVersion
+  }
+}
+rootProject.name = "root-project"
