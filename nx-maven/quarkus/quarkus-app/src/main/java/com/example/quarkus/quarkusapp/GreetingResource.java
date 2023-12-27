0c0ab5f064d21cb6eab59b6f94a7148da99fd7ed
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-12-27 21:31:52 +0100

    work in progress

--- nx-maven/quarkus/quarkus-app/src/main/java/com/example/quarkus/quarkusapp/GreetingResource.java
+++ nx-maven/quarkus/quarkus-app/src/main/java/com/example/quarkus/quarkusapp/GreetingResource.java
@@ -1,5 +1,7 @@
 package com.example.quarkus.quarkusapp;
 
+import com.example.quarkus.quarkuslib.GreetingService;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -8,9 +10,12 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+  @Inject
+  GreetingService service;
+
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
-    return "Hello World!";
+    return service.greeting();
   }
 }
