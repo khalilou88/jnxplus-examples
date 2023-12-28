00306e2cb3e180522c5f91d9ca22246bfdc7fa7b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-12-28 20:27:46 +0100

    work in progress

--- nx-maven/spring-boot/kt-lib/src/test/kotlin/com/example/springboot/ktlib/HelloServiceTests.kt
+++ nx-maven/spring-boot/kt-lib/src/test/kotlin/com/example/springboot/ktlib/HelloServiceTests.kt
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-
 @SpringBootTest
-class HelloServiceTests(@Autowired val helloService: HelloService) {
-
+class HelloServiceTests(
+  @Autowired val helloService: HelloService,
+) {
   @BeforeAll
   fun setup() {
     println(">> Setup")
@@ -22,11 +22,8 @@ class HelloServiceTests(@Autowired val helloService: HelloService) {
     assertThat(helloService.message()).contains("Hello World")
   }
 
-
   @AfterAll
   fun teardown() {
     println(">> Tear down")
   }
-
-
 }
--- nx-maven/spring-boot/kt-lib/src/test/kotlin/com/example/springboot/ktlib/TestConfiguration.kt
+++ nx-maven/spring-boot/kt-lib/src/test/kotlin/com/example/springboot/ktlib/TestConfiguration.kt
@@ -3,5 +3,4 @@ package com.example.springboot.ktlib
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class TestConfiguration {
-}
+open class TestConfiguration
