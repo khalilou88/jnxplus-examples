1dbc79dbd8f90a6b16dc441bc44ccff312eca41e
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-01-12 17:00:26 +0100

    add plugins to root build.gradle.kts

--- nx-gradle/build.gradle.kts
+++ nx-gradle/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
   id("io.github.khalilou88.jnxplus")
+  id("org.springframework.boot") apply false
+  id("io.quarkus") apply false
+  kotlin("jvm") apply false
+  kotlin("plugin.allopen") apply false
 }
