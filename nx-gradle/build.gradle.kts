30af22a35969d18f59ed1f3596afb4f5aa760b9d
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-01-17 23:07:43 +0100

    work in progress

--- nx-gradle/build.gradle.kts
+++ nx-gradle/build.gradle.kts
@@ -1,14 +1,20 @@
 plugins {
   alias(libs.plugins.io.github.khalilou88.jnxplus)
+  // this is necessary to avoid the plugins to be loaded multiple times
+  // in each subproject's classloader
+  // kotlin
+  alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+  alias(libs.plugins.org.jetbrains.kotlin.plugin.allopen) apply false
+  // spring
   alias(libs.plugins.org.springframework.boot) apply false
   alias(libs.plugins.io.spring.dependency.management) apply false
   alias(libs.plugins.org.jetbrains.kotlin.plugin.spring) apply false
+  // quarkus
   alias(libs.plugins.io.quarkus) apply false
-  alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
-  alias(libs.plugins.org.jetbrains.kotlin.plugin.allopen) apply false
-  alias(libs.plugins.com.github.johnrengelman.shadow) apply false
+  // micronaut
   alias(libs.plugins.io.micronaut.application) apply false
   alias(libs.plugins.io.micronaut.library) apply false
   alias(libs.plugins.io.micronaut.aot) apply false
   alias(libs.plugins.com.google.devtools.ksp) apply false
+  alias(libs.plugins.com.github.johnrengelman.shadow) apply false
 }
