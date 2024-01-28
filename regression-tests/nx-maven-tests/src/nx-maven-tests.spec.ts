2b7b2dabaafa2d5a34ae68442521b114dd74f1ac
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-01-28 21:09:29 +0100

    add first test

--- regression-tests/nx-maven-tests/src/nx-maven-tests.spec.ts
+++ regression-tests/nx-maven-tests/src/nx-maven-tests.spec.ts
@@ -1,3 +1,29 @@
+import { normalizePath, workspaceRoot } from "@nx/devkit";
+import { execSync } from "child_process";
+
+function showProjectJson(projectName: string) {
+  return JSON.parse(
+    execSync(`nx show project ${projectName} --json`, {
+      cwd: workspaceRoot,
+      windowsHide: true,
+    })
+      .toString()
+      .trim(),
+  );
+}
+
 describe("nx-maven tests", () => {
-  it("should work", () => {});
+  it("should work", () => {
+    const libName = "build-tools";
+    const projectJson = showProjectJson(libName);
+    expect(Object.entries(projectJson.targets).length).toBe(1);
+    expect(projectJson.targets.build.outputs).toEqual([
+      "{projectRoot}/target",
+      "{options.outputDirLocalRepo}",
+    ]);
+    expect(projectJson.targets.build.options.outputDirLocalRepo).toBeTruthy();
+    expect(
+      normalizePath(projectJson.targets.build.options.outputDirLocalRepo),
+    ).toBe("nx-maven/.m2/repository/com/example/build-tools/1.0");
+  });
 });
