07ebc71de8454d1be142de54399509bbcd6bf676
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-03-03 16:45:43 +0100

    refactor codes

--- nx-maven/spring-boot/m-sb-app/src/test/java/com/example/msbapp/MsbApplicationTests.java
+++ nx-maven/spring-boot/m-sb-app/src/test/java/com/example/msbapp/MsbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MavenAppApplicationTests {
+class MsbApplicationTests {
 
   @Test
   void contextLoads() {}
